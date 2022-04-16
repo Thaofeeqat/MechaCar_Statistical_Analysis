d405eebf60055dc19384e0065e69bab00a5b66ee
Thaofeeqat Dauda <96325903+Thaofeeqat@users.noreply.github.com>
2022-04-16 07:24:41 -0500

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -19,3 +19,12 @@ total_summary <- Suspension_Coil_table %>% summarize(Mean=mean(PSI), Median=medi
 # creating lot_summary using groupby() and summarize()
 lot_summary <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
+# using the t.test() function to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(Suspension_Coil_table$PSI, mu=1500)
+# writing more t.test and its subset() argument to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+lot1 <- subset(Suspension_Coil_table, Manufacturing_Lot == 'Lot1')
+t.test(lot1$PSI, mu=1500)
+lot2 <- subset(Suspension_Coil_table, Manufacturing_Lot == 'Lot2')
+t.test(lot2$PSI, mu=1500)
+lot3 <- subset(Suspension_Coil_table, Manufacturing_Lot == 'Lot3')
+t.test(lot3$PSI, mu=1500)
