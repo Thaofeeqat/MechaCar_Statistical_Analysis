bbdaf6a81bf0411648348b4a5721e1c3a925d50f
Thaofeeqat Dauda <96325903+Thaofeeqat@users.noreply.github.com>
2022-04-16 06:56:19 -0500

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -10,3 +10,12 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Determine p value using summary()
 summary(lm(mechacar_linearregression))
+
+# DELIVERABLE 2
+#Importing the suspension_coil.csv file as table
+Suspension_Coil_table <- read.csv(file='Suspension_Coil.csv')
+# create a total_summary dataframe using the summarize to show the KPIs
+total_summary <- Suspension_Coil_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+# creating lot_summary using groupby() and summarize()
+lot_summary <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
